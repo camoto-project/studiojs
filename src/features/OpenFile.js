438aa8ffd7c6da0e6dcbe85a15f339bf5d5fcfeb
Adam Nielsen <malvineous@shikadi.net>
2021-03-29 22:24:22 +1000

    Implement 'open' button in file selector

--- src/features/OpenFile.js
+++ src/features/OpenFile.js
@@ -23,8 +23,10 @@ import {
 	findHandler as gamearchiveFindHandler,
 } from '@camoto/gamearchive';
 
+import Error from '../components/Error.js';
 import FormatList from '../components/FormatList.js';
 import VirtualUpload from '../components/VirtualUpload.js';
+
 import './OpenFile.css';
 
 const findHandler = {
@@ -43,9 +45,49 @@ function OpenFile(props) {
 	const [ possibleFormats, setPossibleFormats ] = useState(null);
 	const [ chosenFormat, setChosenFormat ] = useState();
 	const [ supps, setSupps ] = useState([]);
+	const [ errorMessage, setErrorMessage ] = useState(null);
+
+	const catFindHandler = findHandler[props.category];
+	const catFormats = allFormats[props.category];
+
+	// User opened the file
+	function onOpen() {
+		setErrorMessage(null);
+		if (!file) {
+			setErrorMessage(`File doesn't seem to have been loaded?`);
+			return;
+		}
+
+		const handler = catFormats.find(h => h.metadata().id === chosenFormat);
+		if (!handler) {
+			setErrorMessage(`Somehow selected a file handler ("${chosenFormat}") that doesn't exist!`);
+			return;
+		}
+
+		let content = {
+			main: file.content,
+		};
+		content.main.filename = file.filename;
+
+		for (const [idSupp, s] of Object.entries(supps)) {
+			content[idSupp] = s.virtualUpload.content;
+			content[idSupp].filename = s.virtualUpload.name;
+		}
+		switch (props.category) {
+			case 'archive':
+				try {
+					const archive = handler.parse(content);
+					props.onOpen(archive);
+				} catch (e) {
+					setErrorMessage(e.message);
+				}
+				break;
+		}
+	}
 
 	// User supplied a main file to use.
 	function onFileChanged(newFile) {
+		setErrorMessage(null);
 		if (!newFile) {
 			setPossibleFormats(null);
 			setChosenFormat(null);
@@ -61,6 +103,7 @@ function OpenFile(props) {
 
 	// User supplied one of the supplementary files we requested.
 	function onSuppChanged(id, newFile) {
+		setErrorMessage(null);
 		if (!supps[id]) {
 			console.error(`Tried to set a file for invalid supp ID "${id}"`);
 			return;
@@ -74,6 +117,7 @@ function OpenFile(props) {
 	// User changed the format suggested by the autodetect, for cases where the
 	// autodetect algorithm matches multiple formats.
 	function onChosenFormatChanged(idNewFormat) {
+		setErrorMessage(null);
 		setChosenFormat(idNewFormat);
 		prepareSupps(idNewFormat, file);
 	}
@@ -81,6 +125,7 @@ function OpenFile(props) {
 	// User changed the manual format dropdown (possibly changing it back to
 	// 'Autodetect').
 	function onManualFormat(idNewFormat) {
+		setErrorMessage(null);
 		setPossibleFormats(null);
 		setManualFormat(idNewFormat);
 		setChosenFormat(idNewFormat);
@@ -95,10 +140,10 @@ function OpenFile(props) {
 	// match the result.  The file passed in can come from the state or from a
 	// value just set by the user that hasn't made it into the state yet.
 	function autodetect(mainFile) {
+		setErrorMessage(null);
 		setManualFormat('auto');
 		let newSupps = {};
 		if (mainFile) {
-			const catFindHandler = findHandler[props.category];
 			let handlers = [];
 			if (catFindHandler) {
 				handlers = catFindHandler(mainFile.content, mainFile.name);
@@ -123,7 +168,6 @@ function OpenFile(props) {
 	// current state.
 	function prepareSupps(targetFormat, mainFile) {
 		let newSupps = {};
-		const catFormats = allFormats[props.category];
 		if (catFormats && mainFile) {
 			const handler = catFormats.find(h => h.metadata().id === targetFormat);
 			if (handler) {
@@ -201,7 +245,7 @@ function OpenFile(props) {
 				</Card.Header>
 
 				<Card.Body>
-					<Form>
+					<Form onSubmit={onOpen}>
 
 						<Form.Item label="File to open">
 							<VirtualUpload
@@ -229,11 +273,23 @@ function OpenFile(props) {
 						{jsxSupps}
 
 					</Form>
+
+					{errorMessage && (
+						<Error summary={`Error`}>
+							{errorMessage}
+						</Error>
+					)}
+
 				</Card.Body>
 
-				<Card.Footer align="right">
+				<Card.Footer>
+					{props.renderCancel && (
+						<span className="cancel">
+							{props.renderCancel}
+						</span>
+					)}
 					<Card.Submit disabled={!openEnabled}>
-						<Icon icon={iconEdit} style={{marginRight: 6, marginBottom: -1}}/>
+						<Icon icon={iconEdit} style={{marginRight: 6, marginBottom: -1}} />
 						Open
 					</Card.Submit>
 				</Card.Footer>
