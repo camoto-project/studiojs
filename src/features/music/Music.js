fb24a2b8a722f987b509510fcc12c9beeefa0b8c
Adam Nielsen <malvineous@shikadi.net>
2021-03-29 22:23:24 +1000

    Add music file selector

--- src/features/music/Music.js
+++ src/features/music/Music.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Link as RRLink } from 'react-router-dom';
 
 import OpenFile from '../OpenFile.js';
 import './Music.css';
 
-function OpenMusic() {
+function Music() {
 	return (
 		<div className="Music">
 			<OpenFile
 				category="music"
 				title="Select a music file"
+				renderCancel={(
+					<RRLink to="/">
+						Cancel
+					</RRLink>
+				)}
 			/>
 		</div>
 	);
 }
 
-export default OpenMusic;
+export default Music;
