e3cbec99457150f434d08cf0828e233644f1fc90
Adam Nielsen <malvineous@shikadi.net>
2021-10-03 13:57:30 +1000

    Game: Add comment header to file

--- src/features/game/Game.js
+++ src/features/game/Game.js
@@ -1,3 +1,25 @@
+/*
+ * Camoto Studio Online - Game
+ *
+ * The <Game/> component provides the list of game items from the parent
+ * <GameHandler/> and a means of opening editors to access each item.
+ *
+ * Copyright (C) 2010-2021 Adam Nielsen <malvineous@shikadi.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React, { useCallback, useEffect, useState } from 'react';
 import {
 	HashRouter as Router,
